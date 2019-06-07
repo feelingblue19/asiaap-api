b41f83445a79c24d8b10cf2412ad69e732a00431
Marcellinus Hari P <marcellinus17@gmail.com>
2019-06-07 23:53:14 +0700

    enable cors

--- app.js
+++ app.js
@@ -12,6 +12,19 @@ app.use(bodyParser.json());
 app.use(morgan('dev'));
 app.use('/public',express.static('public'));
 
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header(
+      "Access-Control-Allow-Headers",
+      "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+    );
+    if (req.method === 'OPTIONS') {
+        res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
+        return res.status(200).json({});
+    }
+    next();
+  });
+
 app.use('/merkkendaraan', merkKendaraanRoutes);
 app.use('/tipekendaraan', tipeKendaraanRoutes);
 app.use('/sparepart', sparepartRoutes);
